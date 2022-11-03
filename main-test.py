751c0cb9cdbcd72c98fe19051ef72b0186cc4fc8
Kushagra Parashar <kush.6542@gmail.com>
2022-11-03 15:55:42 +0530

    checking edge cases

--- main-test.py
+++ main-test.py
@@ -3,7 +3,7 @@ from main import Add
 
 def TestAdd():
         assert Add(21,3) == 24
-        assert Add(1, 3) == 4
+        assert Add(1, 3) == 464
         print("Add Function works correctly")
 
 if __name__ == '__main__':
