148cab16b8d21ee75a7e260b5862f82494b8de67
Kushagra Parashar <kush.6542@gmail.com>
2022-11-03 11:26:39 +0530

    Initial

---
+++ main-test.py
@@ -0,0 +1,8 @@
+from main import Add
+
+def TestAdd():
+        assert Add(21,3) == 24
+        print("Add Function works correctly")
+
+if __name__ == '__main__':
+        TestAdd()
---
+++ main.py
@@ -0,0 +1,10 @@
+def Add(a, b):
+    return a + b
+
+
+def SayHello():
+    print("sup world from srcmake")
+
+
+if __name__ == '__main__':
+    SayHello()
